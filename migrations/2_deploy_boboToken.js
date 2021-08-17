65d4cc0dd26987dc17dca9161a76f51b5cfee779
syslink <oursyslink@163.com>
2021-08-17 15:01:59 +0800

    update

--- migrations/2_deploy_boboToken.js
+++ migrations/2_deploy_boboToken.js
@@ -1,8 +1,8 @@
 
 const BoboToken = artifacts.require("BoboToken");
-const BoboBetaToken = artifacts.require("BoboBetaToken");
+//const BoboBetaToken = artifacts.require("BoboBetaToken");
 
 module.exports = async function(deployer) {
   await deployer.deploy(BoboToken);
-  await deployer.deploy(BoboBetaToken);
+  //await deployer.deploy(BoboBetaToken);
 };
